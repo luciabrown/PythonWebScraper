cedee186cc4313dcb2eea5c6cdeed7a504b8ae39
luciabrown <brownlu@tcd.ie>
2024-05-08 22:54:46 +0100

    Messing with beautifulsoup

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-#dummy test for status code & html using requests
-url ="https://webscraper.io/test-sites/e-commerce/allinone/computers"
-requestedURL = requests.get(url)
-print(requestedURL.status_code)
-print(requestedURL.text)
+url = requests.get("https://webscraper.io/test-sites/e-commerce/allinone/computers")
+print(url.status_code)
 
 #dummy test for status code & html using beautifulsoup
-soup = BeautifulSoup(requestedURL.text,"lxml")
-print(soup)
+soup = BeautifulSoup(url.text,"lxml")           #Init bs
+
+tagDiv = soup.div                                  #set tags
+tagHeader = soup.header
+
+print(tagDiv.attrs)                                #print attributes
+print(tagHeader.attrs)
+print(tagHeader.attrs["class"])
